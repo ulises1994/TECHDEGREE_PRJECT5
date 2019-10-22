bcf2abccf1aeae15bd7a511a76feef8216ba6e8f
ulises1994 <ulises1994@ymail.com>
2019-10-21 23:20:13 -0700

    fixed the search filter

--- js/main.js
+++ js/main.js
@@ -9,20 +9,29 @@ $(".gallery").colorbox({
 
 
 
-
+//creates search filter
 function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, a, i, txtValue, b;
   input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
   li = ul.getElementsByTagName('li');
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  for (i = 0; i < li.length; i++)
+
+
+
+  {
+    //assignd the attributes
+    a = li[i].getElementsByTagName("img")[0].getAttribute("alt");
+    b = li[i].getElementsByTagName("img")[0].getAttribute("title");
+
+
+    //searches through the attributes
+    if ((a.toUpperCase().indexOf(filter) > -1) || (b.toUpperCase().indexOf(filter) > -1)) {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
     }
   }
 }
+//search filter ends
